45b969311ffbe774123d20a7122b70f8e731f451
Natalia-Nazarenko <nazarenkont@gmail.com>
2022-05-10 22:06:52 +0300

    projects section created

--- script/script.js
+++ script/script.js
@@ -8,4 +8,7 @@ $('.slider').slick({
     arrows: false,
 })
 $(".slick-dots").children().children().addClass("dot-btns");
+// $(".slick-dots").children().css("position", "relative");
+// $(".slick-dots").children().children().css("position", "absolute");
+
 
