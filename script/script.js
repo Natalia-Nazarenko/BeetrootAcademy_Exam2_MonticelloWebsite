4be28f1f784203ebd2e71dc6075f3ad4baab199a
Natalia-Nazarenko <nazarenkont@gmail.com>
2022-05-11 13:50:17 +0300

    slider created

--- script/script.js
+++ script/script.js
@@ -6,9 +6,52 @@ $('.slider').slick({
     autoplay: false,
     dots: true,
     arrows: false,
-})
-$(".slick-dots").children().children().addClass("dot-btns");
-// $(".slick-dots").children().css("position", "relative");
-// $(".slick-dots").children().children().css("position", "absolute");
+});
+$(".slider .slick-dots").children().children().addClass("dot-btns");
+
+$('.slider-news').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    dots: true,
+    arrows: true,
+    // responsive: [
+    //     {
+    //         breakpoint: 1024,
+    //         settings: {
+    //             slidesToShow: 3,
+    //             slidesToScroll: 3,
+    //             infinite: true,
+    //             dots: true
+    //         }
+    //     },
+    //     {
+    //         breakpoint: 600,
+    //         settings: {
+    //             slidesToShow: 2,
+    //             slidesToScroll: 2
+    //         }
+    //     },
+    //     {
+    //         breakpoint: 480,
+    //         settings: {
+    //             slidesToShow: 1,
+    //             slidesToScroll: 1
+    //         }
+    //     }
+    // ]
+});
+
+$(".slider-news .slick-dots").children().children().addClass("dot-btns");
+
+
+
+// slider
+// карта
+// футер
+// перехід на секції
+// респонсівнес
+// паралакс
 
 
