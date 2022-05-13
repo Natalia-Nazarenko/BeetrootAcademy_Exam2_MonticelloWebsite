fb3e0de435bcf0755f11f1b4f206ed6b02936353
Natalia-Nazarenko <nazarenkont@gmail.com>
2022-05-14 00:49:42 +0300

    parallax added

--- script/script.js
+++ script/script.js
@@ -16,44 +16,25 @@ $('.slider-news').slick({
     autoplaySpeed: 4000,
     dots: true,
     arrows: true,
-    // responsive: [
-    //     {
-    //         breakpoint: 1024,
-    //         settings: {
-    //             slidesToShow: 3,
-    //         }
-    //     },
-    //     {
-    //         breakpoint: 600,
-    //         settings: {
-    //             slidesToShow: 2,
-    //         }
-    //     },
-    //     {
-    //         breakpoint: 480,
-    //         settings: {
-    //             slidesToShow: 1,
-    //         }
-    //     }
-    // ]
+    responsive: [
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ]
 });
 
 $(".slider-news .slick-dots").children().children().addClass("dot-btns");
 
-let smoothLink = document.querySelector('.header__img img');
-smoothLink.addEventListener('click', function (e) {
-    e.preventDefault();
-    let projectsSection = document.querySelector('.section_projects')
-    projectsSection.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-    });
-});
-
-// перехід на секції
-// валідація форми
-// marker
-// респонсівнес
+// Карта, стилі в mapId
 
 let map;
 function initMap() {
@@ -64,16 +45,77 @@ function initMap() {
             mapId: 'd0ab2965df73818c',
             disableDefaultUI: true,
         });
+    const icon = './img/pin.svg';
+    const marker = new google.maps.Marker({
+        position: { lat: 50.44896759, lng: 30.51332140 },
+        icon: icon,
+        map: map,
+    });
 }
 
 window.initMap = initMap;
 
-let input = document.querySelector('input');
+//Плавний перехід на секції
 
-// input.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     input.toggleClass('input-value');
-// });
+let headerLinks = document.querySelectorAll('.menu__item');
+let newsSection = document.querySelector('.section_news');
+let contactsSection = document.querySelector('.contacts');
+let projectsSection = document.querySelector('.section_projects');
+
+headerLinks.forEach(item => {
+    let linkAttribute = item.getAttribute('data-anchor');
+    item.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (linkAttribute == 'news') {
+            newsSection.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+        else if (linkAttribute == 'contacts') {
+            contactsSection.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+        else {
+            projectsSection.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    })
+});
+
+let smoothLink = document.querySelector('.header__img img');
+
+smoothLink.addEventListener('click', function (e) {
+    e.preventDefault();
+    projectsSection.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    });
+});
+
+// Parallax ефект на картинках Projects Section
+
+let parallax = document.getElementById('parallax');
+let parallaxLeft = new Parallax(parallax, {
+    relativeInput: true,
+    hoverOnly: true,
+});
+
+let parallax2 = document.getElementById('parallax-2');
+let parallaxRight = new Parallax(parallax2, {
+    relativeInput: true,
+    hoverOnly: true,
+});
+
+
+
+
+
+
 
 
 
