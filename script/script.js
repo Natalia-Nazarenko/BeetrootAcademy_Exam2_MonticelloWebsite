b6fa0afcce823df0dceb81220bf778f28527e36f
Natalia-Nazarenko <nazarenkont@gmail.com>
2022-05-17 02:31:21 +0300

    final version

--- script/script.js
+++ script/script.js
@@ -9,26 +9,37 @@ $('.slider').slick({
 });
 $(".slider .slick-dots").children().children().addClass("dot-btns");
 
+let windowWidth = window.innerWidth;
+console.log(windowWidth);
+
 $('.slider-news').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 4000,
+    // autoplay: true,
+    // autoplaySpeed: 4000,
     dots: true,
     arrows: true,
     responsive: [
         {
-            breakpoint: 600,
+            breakpoint: 1024,
             settings: {
                 slidesToShow: 2,
+                slidesToScroll: 1,
+                arrows: false,
+                // autoplay: true,
+                // autoplaySpeed: 4000,
             }
         },
         {
-            breakpoint: 480,
+            breakpoint: 900,
             settings: {
                 slidesToShow: 1,
+                slidesToScroll: 1,
+                // autoplay: true,
+                // autoplaySpeed: 4000,
+                arrows: false,
             }
-        }
+        },
     ]
 });
 
@@ -110,6 +121,40 @@ let parallaxRight = new Parallax(parallax2, {
     relativeInput: true,
     hoverOnly: true,
 });
+
+// Burger Menu
+
+let menu = document.querySelector(".menu");
+let headerSocial = document.querySelector(".header .nav__social");
+
+function burgerMenu() {
+    if (window.innerWidth < 830) {
+        menu.append(headerSocial);
+    }
+}
+burgerMenu();
+
+if (window.innerWidth < 830) {
+    $('.menu').hide();
+}
+$(".burger__btn").click(() => {
+    $('.menu').toggle(200);
+});
+
+//Дані в Local Storage
+
+let mailValue;
+let nameValue;
+let formBtn = document.querySelector('.form__btn');
+formBtn.addEventListener('click', saveUserData);
+
+function saveUserData() {
+    mailValue = document.querySelector('#mail').value;
+    nameValue = document.querySelector('#username').value;
+    localStorage.name = nameValue;
+    localStorage.email = mailValue;
+};
+
 
 
 
